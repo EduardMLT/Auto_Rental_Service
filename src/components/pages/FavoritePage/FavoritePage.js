4cb2bcdaa303fb670202afec5e9d843c37024b2d
EduardMLT <eddku@i.ua>
2023-12-12 01:42:53 +0200

    initial

--- src/components/pages/FavoritePage/FavoritePage.js
+++ src/components/pages/FavoritePage/FavoritePage.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 
 const FavoritePage = () => {
-  return <div>
-    FavoritePage<br/>
-    <Link to="/catalog">Catalog</Link><br/>
-    <Link to="/catalog/autoId">single_Catalog</Link>
-  </div>;
+  return (
+    <div>
+      FavoritePage
+      <br />
+      <Link to="/catalog">Catalog</Link>
+      <br />
+    </div>
+  );
 };
 
 export default FavoritePage;
